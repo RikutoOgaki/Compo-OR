eedfba8ed1857a34134044df8937e5c9125c7c41
Rikuto <orikutobasuke@gmail.com>
2023-08-05 23:09:21 +0900

    Component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,16 +3,19 @@ import dayjs from 'dayjs'
 import style from '@/styles/components/Calendar/calendar.module.scss'
 
 type State = {
+    CalendarSize: number,
 
 }
 
 export default function Calendar() {
 
-    const [state, setState] = useState()
+    const [state, setState] = useState({
+        CalendarSize: 0
+    })
 
     return (
         <>
-            <div className={style.CalendarWrap}>
+            <div className={style.CalendarWrap} style={{ width: `${state.CalendarSize}px`, height: `${state.CalendarSize}px` }}>
 
             </div>
         </>
