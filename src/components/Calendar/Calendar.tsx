1a2dbec0ad68032727e3229bfadf89586ccc20a9
Rikuto <orikutobasuke@gmail.com>
2023-08-05 22:58:05 +0900

    Component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
 import style from '@/styles/components/Calendar/calendar.module.scss'
 
+type State = {
+
+}
+
 export default function Calendar() {
+
+    const [state, setState] = useState()
+
     return (
         <>
             <div className={style.CalendarWrap}>
