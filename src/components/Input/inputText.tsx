b493ddb5c36ae69c8dd66b51224e15d9c3ee03c2
Rikuto <orikutobasuke@gmail.com>
2023-08-14 14:15:54 +0900

    Component

---
+++ src/components/Input/inputText.tsx
@@ -0,0 +1,9 @@
+import style from '@/styles/components/InputText/input.module.scss'
+
+export function InputText() {
+    return (
+        <>
+            <input type="text" className={style.InputText} />
+        </>
+    )
+}
