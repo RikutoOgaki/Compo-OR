f6a0de8cc1523e15d1e12e8661f094ffb5a3bf70
Rikuto <orikutobasuke@gmail.com>
2023-08-15 14:35:53 +0900

    Component

--- src/components/Input/inputText.tsx
+++ src/components/Input/inputText.tsx
@@ -1,5 +1,11 @@
 import style from '@/styles/components/InputText/input.module.scss'
 
+type Props = {
+    inputText: string,
+    inputWidth: string,
+    inputHeight: string
+}
+
 export function InputText() {
     return (
         <>
