a8047610a3b6e3f6bc024c6849a05f342c5c4d48
Rikuto <orikutobasuke@gmail.com>
2023-10-16 11:19:44 +0900

    ModalComponent作成

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,25 @@
+import React, { ReactNode } from 'react'
 import style from '@/styles/components/Modal/modal.module.scss'
 
-export default function Modal() {
+type ModalProps = {
+    isOpen: boolean,
+    isClose: () => void,
+    children: ReactNode
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, isClose, children }) => {
+
+    if (isOpen) return null
+
     return (
-        <></>
+        <>
+            <div>
+                <div>
+                    {children}
+                </div>
+            </div>
+        </>
     )
 }
+
+export default Modal
---
+++ src/pages/Modal/index.tsx
@@ -0,0 +1,9 @@
+import Modal from '@/components/Modal/Modal'
+
+export default function Modal() {
+    return (
+        <>
+
+        </>
+    )
+}
