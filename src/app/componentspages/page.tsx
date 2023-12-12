382a5225ed8dbd67411bac10982aaf7ffac20da0
Rikuto <orikutobasuke@gmail.com>
2023-12-12 22:40:53 +0900

    InputTextComponent作成中

--- src/app/componentspages/page.tsx
+++ src/app/componentspages/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import { InputText } from '@/Components/common/Input/InputText'
+
 export default function HomePage() {
     return (
         <>
             <h1>pageに遷移した</h1>
+            <InputText />
         </>
     )
 }
